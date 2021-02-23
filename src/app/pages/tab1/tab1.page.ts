b65dc685aaa7d6bbf5d27dbfae12fcf4757c809f
Trigalo27 <internick27@gmail.com>
2021-02-23 11:33:46 -0600

    comit a deseos

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,18 +1,58 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page { 
 
-  constructor( private deseosService: DeseosService, private router: Router ) {}
+  constructor( private deseosService: DeseosService, private router: Router, private alert: AlertController ) {}
 
-  agregarLista() {
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista() {
+
+    const alert = await this.alert.create({
+      header: 'Nueva Lista',
+      inputs: [
+        { name: 'titulo', type: 'text', placeholder: 'Nombre de la lista' }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }, {
+          text: 'Crear',
+          handler: ( data ) => {
+            console.log(data);
+            if( data.titulo.length === 0 ){
+              return;
+            }
+
+            const listaId = this.deseosService.crearLista( data.titulo );
+            //Tengo que crear la lista
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
+  
 }
+
+
+
+
+
+
+
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,10 +9,49 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() { 
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a vencer');
-
-    this.listas.push( lista1, lista2 );
+    this.cargarStorage();
+    // const lista1 = new Lista('Recolectar piedras del infinito');
+    // const lista2 = new Lista('Heroes a vencer');
+    // this.listas.push( lista1, lista2 );
 
   }
+
+
+  crearLista( titulo: string) {
+    const nuevaLista = new Lista( titulo );
+    this.listas.push( nuevaLista );
+    this.guardarStorage();
+
+    return nuevaLista.id;
+  }
+
+  obternerLista( id:string | number ) {
+    id = Number(id);
+
+    return this.listas.find( listaData => listaData.id === id );
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    if(localStorage.getItem('data')){
+      this.listas = JSON.parse( localStorage.getItem('data') );
+    } else {
+      this.listas = [];
+    }
+  }
+
+  borrarLista( lista: Lista) {
+    this.listas = this.listas.filter( listaData => listaData.id !== lista.id );
+    this.guardarStorage();
+  }
+
 }
+
+
+
+
+
+
